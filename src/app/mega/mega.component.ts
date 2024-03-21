3b65a69f3ef4dfcb10f10072ef33759362b7da1e
Vinicius <legithacking069@gmail.com>
2024-03-21 16:10:42 -0300

    mega component added number checker and check numbers

--- src/app/mega/mega.component.ts
+++ src/app/mega/mega.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrl: './mega.component.css'
 })
 export class MegaComponent {
+  numerosSorteados: number[] = [];
+  numerosUsuario: string = '';
+  resultado: number | null = null;
 
+  gerarNumerosSorteados() {
+    while (this.numerosSorteados.length < 6) {
+      const numeroAleatorio = Math.floor(Math.random() * 60) + 1;
+      if (!this.numerosSorteados.includes(numeroAleatorio)) {
+        this.numerosSorteados.push(numeroAleatorio);
+      }
+    }
+  }
+
+  verificarNumeros() {
+    const numerosUsuarioArray = this.numerosUsuario.split(',').map(num => parseInt(num.trim()));
+
+  if (numerosUsuarioArray.length < 6 || numerosUsuarioArray.length > 10) {
+    alert('Por favor, informe entre 6 e 10 números válidos.');
+    return;
+  }
+  this.resultado = numerosUsuarioArray.filter(num => this.numerosSorteados.includes(num)).length;
+}
 }
